bba77a27689b0109b897e801fe7f01d172c5013c
rithaven <veneruth4@gmail.com>
2023-07-11 18:03:52 +0200

    tesk Checkbox

--- cypress/e2e/1-getting-started/myFirstTest.cy.js
+++ cypress/e2e/1-getting-started/myFirstTest.cy.js
@@ -54,10 +54,49 @@ describe("My First Test", () => {
   //   cy.get('h1').should('contain.text', 'Why Cypress?');
   // });
 
-  it('types into an email fiel', () =>{
+  // it('types into an email fiel', () =>{
+  //   cy.visit('https://Example.cypress.io/commands/actions')
+  //   cy.findByPlaceholderText('Email').type('test@email.com')
+  //   cy.wait(2000).then(() =>{
+  //     fetch('https://api.spacexdata.com/v3/missions')
+  //     .then((res) => res.json())
+  //     .then((data) =>{
+  //       //eslint-disable-next-line no-console
+  //       console.log(data)
+  //     })
+  //   })
+  //   console.log('test is finished')
+  // })
+
+  // it('shows an active class for the current page', () =>{
+  //   cy.visit('https://Example.cypress.io/commands/actions')
+  //   cy.get('.dropdown-menu').find('li').eq(2).should('have.class', 'active')
+  // })
+
+  // it('should not have an active class on inactive pages', () =>{
+  //   cy.visit('https://Example.cypress.io/commands/actions')
+  //   cy.get('.dropdown-menu').find('li').first().should('not.have.class', 'active').find('a').should('have.attr', 'href', '/commands/querying')
+  // })
+  it('links to the actions page correctly', () =>{
     cy.visit('https://Example.cypress.io/commands/actions')
-    cy.findByPlaceholderText('Email').type('test@email.com')
-    cy.wait(5000)
-    console.log('test is finished')
+    cy.findAllByText('Actions').first().click({force:true})
+    cy.url().should('include', 'commands/actions')
+  })
+
+  it('lets you type in an input field', () =>{
+    cy.visit('https://Example.cypress.io/commands/actions')
+    cy.findByPlaceholderText('Email').type('Test').should('have.value', 'Test')
+  })
+
+  it('lets you clear an input field', () => {
+    cy.visit('https://Example.cypress.io/commands/actions')
+    cy.findByLabelText('Describe:').type('Test description').should('have.value', 'Test description')
+    .clear().should('have.value', '')
+  })
+
+  it('lets you check a checkbox', () =>{
+    cy.visit('https://Example.cypress.io/commands/actions')
+    cy.get('.action-checkboxes [type="checkbox"]').eq(1).check({force: true}).should('be.checked')
+
   })
 })
