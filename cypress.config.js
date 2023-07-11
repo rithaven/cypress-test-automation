e2084b904e1375ae749f929b5d82d1c592dbd80c
rithaven <veneruth4@gmail.com>
2023-07-11 10:16:58 +0200

    Added 4 Tests

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  'projectId': '4b7344',
+  e2e: {
+    baseUrl: "http://localhost:8080",
+    // defaultCommandTimeout: 9000,
+    env: {
+      navbarText: "cypress.io",
+    },
+  },
+}
--- cypress/e2e/1-getting-started/myFirstTest.cy.js
+++ cypress/e2e/1-getting-started/myFirstTest.cy.js
@@ -1,25 +1,60 @@
 /// <reference types="cypress" />
+
+/// <reference types="cypress" />
+
 describe("My First Test", () => {
   beforeEach(() => {
-    cy.visit("https://docs.cypress.io/guides/overview/why-cypress");
+    cy.visit("https://Example.cypress.io/commands/actions");
   });
+
+  
   it("has an h1 on the page", () => {
-    cy.get("h1").should("exist");
+    cy.get("h1").should("exist")
   });
-  it('renders the correct h1 text', () =>{
-    cy.get('h1').should('container.text','Actions')
-  })
-  it('renders an p under the h1', () =>{
-    cy.get('.container').find('p').should('exist')
-  })
-  it('renders an paragaph  under the h1', () =>{
+
+  it('renders the correct h1 text', () => {
+    cy.get('h1').should('contain.text', 'Actions')
+  });
+  
+  it('renders a paragraph under the h1', () => {
     cy.get('.container').eq(1).find('p').should('exist')
-  })
-  it('renders a section with the correct elements', () =>{
-    cy.get('.container').eq(2).within(() =>{
-      cy.get('h4').should('exist');
-      cy.get('p').should('exist');
-      
+  });
+
+  it('renders a section with the correct elements', () => {
+    cy.get('.container').eq(2).within(()=>{
+      cy.get('h4').should('exist')
+      cy.get('p').should('exist')
     })
   })
+
+  it('renders a p under the h1', () => {
+    cy.get('h1').next('p', { timeout: 10000 }).should('exist');
+  });
+
+  
+
+  // it('renders a section with the correct elements', () => {
+  //   cy.get('.home-landing', { timeout: 10000 }).within(() => {
+  //     cy.get('h2', { timeout: 10000 }).should('exist');
+  //     cy.get('p', { timeout: 10000 }).should('exist');
+  //   });
+  // });
+
+
+
+
+  // it("has an h1 on the page", () => {
+  //   cy.get("h1").should("exist");
+  // });
+
+  // it('renders the correct h1 text', () => {
+  //   cy.get('h1').should('contain.text', 'Why Cypress?');
+  // });
+
+ 
+
+
+  // it('correctly renders the Cypress website link', () => {
+  //   cy.contains('cypress.io').should('exist');
+  // });
 });
